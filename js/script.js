809a1d756b5f4c705df77191bf7cf6e5267779d0
Dima1Garbar <dima.qarbar@gmail.com>
2026-01-27 19:46:57 +0200

    added carousel

--- js/script.js
+++ js/script.js
@@ -6,6 +6,48 @@ const submitModal = document.getElementById('modalSubmit');
 const form = document.getElementById('registerForm');
 const eye = document.getElementById('passwordEye');
 const modalPassword = document.getElementById('modalPassword');
+const clientsLogos = document.getElementById('clientsLogo');
+const bigWindow = window.matchMedia("(min-width: 770px)");
+const mediumWindow = window.matchMedia("(min-width: 480px) and (max-width: 769px)");
+const smallWindow = window.matchMedia("(max-width: 479px)");
+const carouselBtn = document.querySelector(".carousel--button");
+const leftButtom =  document.querySelector(".carousel__button-left");
+const rightButtom =  document.querySelector(".carousel__button-right");
+
+
+let imageList = [{
+    "id": 1,
+    "url": "./img/Logo1.png"
+  },
+  {
+    "id": 2,
+    "url": "./img/Logo2.png"
+  },
+  {
+    "id": 3,
+    "url": "./img/Logo3.png"
+  },
+  {
+    "id": 4,
+    "url": "./img/Logo4.png"
+  },
+  {
+    "id": 5,
+    "url": "./img/Logo5.png"
+  },
+  {
+    "id": 6,
+    "url": "./img/Logo6.png"
+  },
+    {
+    "id": 7,
+    "url": "./img/Logo7.png"
+  }
+
+]
+
+let startIndex = 0;
+let visibleCount = 0;
 
 
 eye.addEventListener('click', () => {
@@ -60,3 +102,60 @@ form.addEventListener('submit', (e) => {
 
   console.log(data)
 });
+
+ 
+function renderLogos() {
+  updateVisibleCount();        
+  clientsLogos.innerHTML = "";
+
+  if (visibleCount >= imageList.length) {
+    carouselBtn.style.display = "none";
+  } else {
+    carouselBtn.style.display = "flex";
+  }
+
+  for (let i = 0; i < visibleCount; i++) {
+    const index = (startIndex + i) % imageList.length;
+
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+
+    li.className = "u-logo";
+    img.src = imageList[index].url;
+    img.alt = "image";
+    img.id = `image-${index + 1}`;
+
+    li.appendChild(img);
+    clientsLogos.appendChild(li);
+  }
+}
+
+
+function updateVisibleCount() {
+  if (bigWindow.matches) visibleCount = imageList.length;
+  else if (mediumWindow.matches) visibleCount = 4;
+  else visibleCount = 3;
+}
+
+
+function switchCarousel(direction) {
+  updateVisibleCount();
+
+  if (direction === "left") {
+    startIndex = (startIndex - 1 + imageList.length) % imageList.length;
+  } else {
+    startIndex = (startIndex + 1) % imageList.length;
+  }
+
+  renderLogos();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderLogos();
+});
+
+bigWindow.addEventListener("change", renderLogos);
+mediumWindow.addEventListener("change", renderLogos);
+smallWindow.addEventListener("change", renderLogos);
+leftButtom.addEventListener("click", () => switchCarousel("left"));
+rightButtom.addEventListener("click", () => switchCarousel("right"));
